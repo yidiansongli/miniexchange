dd7fb975f629f067c2df9765c8f4769a753448d8
xiejing <40300458@qq.com>
2021-11-03 16:19:30 +0800

    商品价格货币化显示

--- pages/exchange/custom/custom.js
+++ pages/exchange/custom/custom.js
@@ -58,7 +58,7 @@ Page({
 
     onShow: function () {
         const version = wx.getSystemInfoSync().SDKVersion;
-        if (this.compareVersion(version, '2.8.3') >= 0) {
+        if (this.service.compareVersion(version, '2.8.3') >= 0) {
             wx.hideHomeButton();
         }
     },
@@ -198,31 +198,5 @@ Page({
         wx.navigateTo({
             url: `/miniexchange/pages/exchange/agreement/agreement?cardid=${cardid}`
         })
-    },
-
-    compareVersion: function (v1, v2) {
-        v1 = v1.split('.')
-        v2 = v2.split('.')
-        const len = Math.max(v1.length, v2.length)
-
-        while (v1.length < len) {
-            v1.push('0')
-        }
-        while (v2.length < len) {
-            v2.push('0')
-        }
-
-        for (let i = 0; i < len; i++) {
-            const num1 = parseInt(v1[i])
-            const num2 = parseInt(v2[i])
-
-            if (num1 > num2) {
-                return 1
-            } else if (num1 < num2) {
-                return -1
-            }
-        }
-
-        return 0
-    },
+    }
 });
