ab5b8182f73976629ed5a57a48b290a3356ae2ba
xiejing <40300458@qq.com>
2021-11-15 16:15:12 +0800

    商品价格货币化显示

--- pages/exchange/order/address.js
+++ pages/exchange/order/address.js
@@ -6,21 +6,19 @@ var page = {
      * 页面的初始数据
      */
     data: {
-        cardid: '',
         orderids: '',
-        name: "",
+        address: {
+            name: "",
+            telNumber: "",
+            telNumber2: "",
+            address: "",
+            region: ['请选择', '请选择', '请选择'],
+        },
         postalCode: "",
-        telNumber: "",
         area: "",
-        address: "",
-        region: ['请选择', '请选择', '请选择'],
         customItem: '请选择',
         showinfo: false,
         showpage: false,
-        showrange: true,
-        modifyaddress: 0,
-        showcurrency: false,
-        norange: false,
         addressdetail: false,
         subscribe: true,
         clickNum: 0
@@ -39,11 +37,11 @@ var page = {
                 success: function (res) {
                     var region = [res.provinceName, res.cityName, res.countyName];
                     that.setData({
-                        region: region,
-                        name: res.userName,
-                        postalCode: res.postalCode,
-                        telNumber: res.telNumber,
-                        address: res.detailInfo,
+                        ["address.region"]: region,
+                        ["address.name"]: res.userName,
+                        ["address.postalCode"]: res.postalCode,
+                        ["address.telNumber"]: res.telNumber,
+                        ["address.address"]: res.detailInfo,
                         showpage: true
                     })
                 }, fail: function () {
@@ -60,13 +58,22 @@ var page = {
     bindRegionChange: function (e) {
         //console.log('picker发送选择改变，携带值为', e.detail.value)
         this.setData({
-            region: e.detail.value
+            ['address.region']: e.detail.value
         })
     },
 
 
     formsubmit: function (e) {
-        this.apply(e.detail.value);
+        let data = e.detail.value;
+        console.log(data);
+        this.setData({
+            ["address.name"]: data.name,
+            ["address.telNumber"]: data.telNumber,
+            ["address.telNumber2"]: data.telNumber2,
+            ["address.address"]: data.address,
+        }, () => {
+            this.apply(data);
+        });
     },
 
     apply: function (data) {
@@ -74,11 +81,12 @@ var page = {
         var showinfo = this.data.showinfo;
         var name = data.name;
         var telNumber = data.telNumber;
-        var region = data.region;
+        var telNumber2 = data.telNumber2;
+        var region = this.data.address.region;
         var address = data.address;
-        var provinceName = that.data.region[0]; //国标收货地址第一级地址
-        var cityName = that.data.region[1];     //国标收货地址第二级地址
-        var countyName = that.data.region[2];   //国标收货地址第三级地址
+        var provinceName = region[0]; //国标收货地址第一级地址
+        var cityName = region[1];     //国标收货地址第二级地址
+        var countyName = region[2];   //国标收货地址第三级地址
         var orderids = that.data.orderids;
         if (name == "" || telNumber == "" || region == "" || address == "" || provinceName == "请选择" || cityName == "请选择" || countyName == "请选择") {
             this.service.toastPromise('请完善信息后重试', 'none', 1500);
@@ -87,11 +95,13 @@ var page = {
         } else if (!showinfo) {
             this.setData({showinfo: true});
         } else {
+            wx.showLoading({mask: true});
             this.service.postPromise('/partner/order/modify?cardid=' + this.options.cardid,
                 {
-                    orderids:orderids,
+                    orderids: orderids,
                     name: name,
                     telNumber: telNumber,
+                    telNumber2: telNumber2,
                     area: provinceName + '|' + cityName + '|' + countyName,
                     address: address
                 }
@@ -100,19 +110,17 @@ var page = {
                     wx.redirectTo({
                         url: '/miniexchange/pages/exchange/order/redirect?cardid=' + this.options.cardid,
                     });
-                } else if (code == 1620) {
-                    this.setData({norange: true, showinfo: false, showcurrency: true});
-                } else if (code == 3405) {
-                    this.setData({showinfo: false}, () => this.addressdetail());
                 } else {
                     this.setData({showinfo: false, message: res.message});
                 }
+            }).finally(() => {
+                wx.hideLoading();
             })
         }
     },
 
     addressdetail: function () {
-        app.func.toastPromise('亲，详细地址填写有误，请重新填写。', 'none', 2000)
+        this.service.toastPromise('亲，详细地址填写有误，请重新填写。', 'none', 2000)
     },
 
     hideinfo: function () {
@@ -126,19 +134,5 @@ var page = {
     onShow: function () {
         wx.hideHomeButton();
     },
-
-
-    view_range: function () {
-        var rangelength = this.data.range.length;
-        if (rangelength > 3) {
-            this.setData({showallrange: true});
-        }
-    },
-    hideallrange: function () {
-        this.setData({showallrange: false});
-    },
-    hiderange: function () {
-        this.setData({showrange: false});
-    }
 };
 Page(page);
